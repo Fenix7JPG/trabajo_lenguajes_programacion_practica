321909b849d11632fe66422f6faa1072988c4fa3
fenix7jpg <fenix7jpg@gmail.com>
2026-09-03 20:09:16 -0500

    estilizadno

--- ejercicios/ej2/clase.py
+++
@@ -1,9 +0,0 @@
-class Persona:
-    # El constructor inicializa los datos de la persona
-    def __init__(self, nombre, edad):
-        self.nombre = nombre  # Atributo
-        self.edad = edad      # Atributo
-
-    # Método para que la persona realice una acción
-    def saludar(self):
-        return f"Hola, mi nombre es {self.nombre} y tengo {self.edad} años."
